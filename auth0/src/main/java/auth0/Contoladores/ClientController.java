170673f8f85004d62b9068c6b2993675a9fd4d68
IgnaValdivia <129917498+IgnaValdiviaUTN@users.noreply.github.com>
2024-05-30 13:26:43 -0300

    Add files via upload

---
+++ auth0/src/main/java/auth0/Contoladores/ClientController.java
@@ -0,0 +1,19 @@
+package auth0.Contoladores;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/client", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ClientController {
+
+    @GetMapping("")
+    public ResponseEntity<?> publicEndpoint() {
+        return ResponseEntity.status(HttpStatus.OK).body("Endpoint usuario. " +
+                "autenticado");
+    }
+}
